2006026695a0016f3fca4580bb40507cffc3867b
Abdiel Alvarez Rojas <abdielalvarez@icloud.com>
2024-03-05 18:45:52 -0600

    improvements

--- js/index.js
+++ js/index.js
@@ -8,10 +8,77 @@ const noteResultButton = document.getElementById('note-result-button');
 const noteResult = document.getElementById('note-result');
 const acousticResultButton = document.getElementById('acoustic-result-button');
 const acousticResult = document.getElementById('acoustic-result');
+const ctxContainer = document.getElementById('chart-container');
+const ctx = document.getElementById('chart');
 const cromaticScale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+let frequencies = []
+let decibels = []
+
+const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: frequencies,
+        datasets: [{
+            label: 'Decibels',
+            data: decibels,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const generateBarColors = (decibels) => {
+    const colors = [];
+    const maxDecibelIndex = decibels.indexOf(Math.max(...decibels));
+    for (let i = 0; i < decibels.length; i++) {
+        if (i === maxDecibelIndex) {
+            colors.push('#ff0000');
+        } else {
+            const hue = (i / decibels.length) * 360;
+            const color = `hsl(${hue}, 80%, 80%)`;
+            colors.push(color);
+        }
+    }
+    return colors;
+};
+
+const sortParsedObject = (parsedObject) => {
+    parsedObject.sort((a, b) => {
+        if (a.acousticIndex !== b.acousticIndex) {
+            return a.acousticIndex - b.acousticIndex;
+        } else {
+            return cromaticScale.indexOf(a.note) - cromaticScale.indexOf(b.note);
+        }
+    });
+    return parsedObject;
+};
+
+const updateChartData = (frequencies, decibels) => {
+    if (frequencies.length > 0) {
+        chart.data.labels = frequencies;
+        chart.data.datasets[0].data = decibels;
+        const colors = generateBarColors(decibels);
+        chart.data.datasets[0].backgroundColor = colors;
+        chart.data.datasets[0].borderColor = 'rgba(0, 0, 0, 0)';
+        chart.options.plugins.legend.labels.boxWidth = 0;
+        ctxContainer.style.display = 'block';
+        chart.update();
+    } else {
+        ctxContainer.style.display = 'none';
+    }
+};
+updateChartData([], []);
 
 const acousticSoundToHertz = async () => {
     try {
+        updateChartData([], []);
         acousticResultButton.classList.add('blink');
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         const AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -23,7 +90,9 @@ const acousticSoundToHertz = async () => {
         const dataArray = new Uint8Array(bufferLength);
         let maxDecibel = -Infinity;
         let maxFrequency = 0;
+        const maxFrequencies = [];
         audioStream.connect(analyser);
+
         const analyzeFrequencies = () => {
             analyser.getByteFrequencyData(dataArray);
             for (let i = 0; i < bufferLength; i++) {
@@ -34,11 +103,12 @@ const acousticSoundToHertz = async () => {
                 }
             }
         };
+
         let capturaActiva = true;
         const tiempoInicial = Date.now();
         while (capturaActiva) {
             analyzeFrequencies();
-            if (Date.now() - tiempoInicial >= 5000) {
+            if (Date.now() - tiempoInicial >= 3500) {
                 capturaActiva = false;
             }
             await new Promise(resolve => setTimeout(resolve, 100));
@@ -53,12 +123,49 @@ const acousticSoundToHertz = async () => {
             Musical Note ${note}${acousticIndex}
         `;
         acousticResult.innerHTML = text;
+
+        const frequenciesWithDecibels = [];
+        for (let i = 0; i < bufferLength; i++) {
+            const decibel = dataArray[i];
+            frequenciesWithDecibels.push({ frequency: i * audioContext.sampleRate / analyser.fftSize, decibel });
+        }
+        frequenciesWithDecibels.sort((a, b) => b.decibel - a.decibel);
+        maxFrequencies.push(...frequenciesWithDecibels.slice(0, 7));
+        maxFrequencies.sort((a, b) => a.frequency - b.frequency);
+        maxFrequencies.splice(7);
+
+        const parsedObject = maxFrequencies.map(freqObject => {
+            const { frequency, decibel } = freqObject
+            const fullNote = getMusicalNote(frequency);
+            const { note, acousticIndex } = parseMusicalNote(fullNote)
+            return {
+                fullNote,
+                note,
+                acousticIndex,
+                frequency,
+                decibel
+            }
+        })
+        const sortedObject = sortParsedObject(parsedObject) 
+        frequenciesInfo = sortedObject.map(entry => {
+            return `${entry.frequency.toFixed(2)} (${entry.fullNote})`
+        });
+        decibels = sortedObject.map(entry => entry.decibel);
+        updateChartData(frequenciesInfo, decibels);
+
     } catch (error) {
-        const text = `
+        let text = `
             There was an error trying to get the sound to hertz,
             could you try to put your audio closer?`;
+        if (
+            error.message.includes('Permission denied') ||
+            error.message.includes('Permission dismissed')
+        ) {
+            text = 'You need to allow the microphone to detect the Hertz'
+            alert('You need to allow the microphone to detect the Hertz in the browser settings')
+        }
         acousticResult.textContent = text;
-        console.error('Error al acceder al micrófono:', error);
+        console.error(error.message);
     } finally {
         acousticResultButton.classList.remove('blink');
     }
@@ -103,7 +210,6 @@ const handleGetFrecuency = (event) => {
     const noteWithIndex = `${note}${index}`
     const calculated = calculateFrequency(noteWithIndex, frequencyReference);
     const result = String(calculated.toFixed(2));
-    frequencyInput.value = result
     frequencyResult.textContent = `${result} Hz`
 };
 
@@ -112,8 +218,6 @@ const handleGetMusicalNote = (event) => {
     const frequency = frequencyInput.value;
     const fullNote = getMusicalNote(frequency);
     const { note, acousticIndex } = parseMusicalNote(fullNote)
-    noteSelect.value = note;
-    indexSelect.value = String(acousticIndex);
     noteResult.textContent = `${note}${acousticIndex} Musical Note`
 };
 
